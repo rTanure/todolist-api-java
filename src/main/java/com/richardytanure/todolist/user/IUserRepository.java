c7427aae7ee8de4d600acc7878d68b2409cfa43d
unknown <richardy.tanure@hotmail.com>
2023-10-13 01:31:16 -0300

    first commit

---
+++ src/main/java/com/richardytanure/todolist/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.richardytanure.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  // Cria um metodo que busca o usuario pelo username
+  UserModel findByUsername(String username);
+}
